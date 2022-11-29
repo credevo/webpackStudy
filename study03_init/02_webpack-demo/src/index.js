dc4cac570fc3113c91e00acc78c80a80a353e183
credevo <62835973+credevo@users.noreply.github.com>
2022-11-29 11:40:49 +0900

    Loading css

--- study03_init/02_webpack-demo/src/index.js
+++ study03_init/02_webpack-demo/src/index.js
@@ -1,7 +1,12 @@
 import _ from "lodash";
+import "./style.css";
+
 function component() {
   const element = document.createElement("div");
+  //loadsh
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  //css import
+  element.classList.add("hello");
   return element;
 }
 
