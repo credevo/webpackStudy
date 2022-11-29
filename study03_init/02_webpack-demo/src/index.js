1e0113142d45036129e55962d2ea3589313be36e
credevo <62835973+credevo@users.noreply.github.com>
2022-11-30 00:49:33 +0900

    loading images

--- study03_init/02_webpack-demo/src/index.js
+++ study03_init/02_webpack-demo/src/index.js
@@ -1,12 +1,20 @@
 import _ from "lodash";
 import "./style.css";
+import icon from "./icon.png";
 
 function component() {
   const element = document.createElement("div");
-  //loadsh
+
+  // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  //css import
   element.classList.add("hello");
+
+  // 원래 있던 div 에 이미지를 추가합니다.
+  const myIcon = new Image();
+  myIcon.src = icon;
+
+  element.appendChild(myIcon);
+
   return element;
 }
 
